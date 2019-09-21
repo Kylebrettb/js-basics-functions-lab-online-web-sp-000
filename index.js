dd6e674ae16c92320391896348fb9ef88195535a
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-21 21:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
+}
